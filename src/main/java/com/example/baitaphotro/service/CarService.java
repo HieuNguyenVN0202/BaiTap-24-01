099f8d5a268caa22dbb10fe68305b78776bff66c
HieuNguyenVN0202 <hieuhnlm1@gmail.com>
2024-01-24 21:12:39 +0700

    Bai tap Car

---
+++ src/main/java/com/example/baitaphotro/service/CarService.java
@@ -0,0 +1,30 @@
+package com.example.baitaphotro.service;
+
+import com.example.baitaphotro.model.Car;
+import com.example.baitaphotro.repository.CarRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CarService {
+    @Autowired
+    private CarRepository carRepository;
+
+    public List<Car> getCarList() {
+        return carRepository.getCarList();
+    }
+
+    public void createCar(Car car) {
+        carRepository.createCar(car);
+    }
+
+    public Car getCarById(int id) {
+        return carRepository.getCarById(id);
+    }
+
+    public void updateCar(Car car){
+        carRepository.updateCar(car);
+    }
+}
